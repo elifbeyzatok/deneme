15110a8c4f795ce33a771909ea653f508eebe390
elifbeyzatok <91954153+elifbeyzatok@users.noreply.github.com>
2021-12-19 14:27:45 +0100

    commited iteration3

---
+++ iteration 3 - Kopya/src/Appointment.java
@@ -0,0 +1,38 @@
+
+public class Appointment {
+	private String id;
+	private String date;
+	private String time;
+	private int patient;
+	private String doctor;
+	private String clinic;
+	private String hospital;
+	private String city;
+
+	public void getSingleAppointment() {
+		// TODO - implement Appointment.getSingleAppointment
+		throw new UnsupportedOperationException();
+	}
+
+	public void getAllAppointment() {
+		// TODO - implement Appointment.getAllAppointment
+		throw new UnsupportedOperationException();
+	}
+
+	public void makeAppointment() {
+		// TODO - implement Appointment.makeAppointment
+		throw new UnsupportedOperationException();
+	}
+
+	public void updateAppointment() {
+		// TODO - implement Appointment.updateAppointment
+		throw new UnsupportedOperationException();
+	}
+
+	public void deleteAppointment() {
+		// TODO - implement Appointment.deleteAppointment
+		throw new UnsupportedOperationException();
+	}
+	
+
+}
